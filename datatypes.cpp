a5b1d7ef3c3925591e069f76b270af293965128c
Swastik <swastikbais2610@gmail.com>
2024-08-08 23:28:56 +0530

    datatypes program

---
+++ datatypes.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int a=14, b=21;
+    float pi=3.1427;
+    char fam='S';
+    bool is_true=true;
+
+    cout<<"the value stored in a is "<<a<<" and the value stored in b is "<<b;
+    cout<<"\n the value of pi is "<<pi;
+    cout<<"\n my family initial is "<<fam<<is_true;
+
+}
